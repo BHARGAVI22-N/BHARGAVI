16e998ff0b97af9de8c0d8286bc097df6fd689fe
BHARGAVI22-N <66381663+BHARGAVI22-N@users.noreply.github.com>
2020-06-04 17:34:30 +0530

    Create Product.cpp

---
+++ Product.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter the two numbers";
+    cin>>a>>b;
+    c=a*b;
+    cout<<"the product of two numbers is c"<<c;
+    return 0;
+}
