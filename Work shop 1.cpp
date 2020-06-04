60952c453bcb7afb87fc214135a1eba5fb0306e3
BHARGAVI22-N <66381663+BHARGAVI22-N@users.noreply.github.com>
2020-06-04 17:58:12 +0530

    Update and rename Product.cpp to Work shop 1.cpp

---
+++ Work shop 1.cpp
@@ -0,0 +1,25 @@
+2)
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b,c;
+    cout<<"enter the two numbers";
+    cin>>a>>b;
+    c=a*b;
+    cout<<"the product of two numbers is c"<<c;
+    return 0;
+}
+
+
+1)
+#include<iostream>
+using namespace std;
+int main ()
+{
+   cout<<"size of char"<<"size of (char)"<<bites;
+   cout<<"size of int"<<"size of ( int)"<<bites;
+   cout<<"size of float"<<"size of ( float)"<<bites;
+   cout<<"size of double"<<"size of(double)"<<bites;
+   return 0;
+}
